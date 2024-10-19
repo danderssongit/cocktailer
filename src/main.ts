d024cfe7b546c814a66e0d25c4aae8a980248f3f
Daniel Andersson <dandersson23@gmail.com>
2024-10-19 15:49:39 +0200

    my first lit component

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log("Hello World");
+import { LitElement, html } from "lit";
+import { property, customElement } from "lit/decorators.js";
+
+@customElement("cocktail-app")
+export class CocktailApp extends LitElement {
+	@property()
+	name = "world";
+
+	render() {
+		return html`<h1>Hello ${this.name}</h1>`;
+	}
+}
+
+@customElement("counter-button")
+export class CounterButton extends LitElement {
+	@property()
+	count = 0;
+
+	addOne() {
+		this.count++;
+	}
+
+	render() {
+		return html`<button @click=${this.addOne}>Click me ${this.count}</button>`;
+	}
+}
